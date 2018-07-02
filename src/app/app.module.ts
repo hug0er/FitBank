7e0b2c02634fd8fbcb14c16acd719632613fbc71
mau0810 <mauricio.aguilera@yachaytech.edu.ec>
2018-07-02 15:36:57 -0500

    Anadir home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
