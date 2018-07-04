2af6025f4a55d85a67339b9fd6d44d0ad5166b03
mau0810 <mauricio.aguilera@yachaytech.edu.ec>
2018-07-04 15:51:01 -0500

    Cuarto commit de home

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,7 +9,8 @@ import {
   MatInputModule,
   MatTabsModule,
   MatButtonModule,
-  MatSelectModule
+  MatSelectModule,
+  MatDatepickerModule
 } from '@angular/material';
 
 @NgModule({
@@ -22,7 +23,8 @@ import {
     MatInputModule,
     MatTabsModule,
     MatButtonModule,
-    MatSelectModule
+    MatSelectModule,
+    MatDatepickerModule
   ],
   exports: [
     MatSidenavModule,
@@ -33,7 +35,8 @@ import {
     MatInputModule,
     MatTabsModule,
     MatButtonModule,
-    MatSelectModule
+    MatSelectModule,
+    MatDatepickerModule
   ]
 })
 export class MaterialModule {}
