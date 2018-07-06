60615d99754fe71b139c5b5292ca69021827c3b5
hug0er <hugo.e.betancourt@gmail.com>
2018-07-06 10:10:10 -0500

    push

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -14,7 +14,6 @@ export class ApiService {
   constructor( private http: HttpClient) {
 
   }
-
   createHeader () {
     let header = new HttpHeaders();
          header = header.append('Content-Type','application/json');
